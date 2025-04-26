4bfa0c1c92e46c8d3ea7e55f65cfa28c9a66c6eb
isgust <rocha.gustavops@gmail.com>
2025-04-26 16:34:13 -0300

    metodo de adicionar contato

--- src/main/java/com/gustavo/contatos/model/ContatoDAO.java
+++ src/main/java/com/gustavo/contatos/model/ContatoDAO.java
@@ -1,4 +1,30 @@
 package com.gustavo.contatos.model;
+import java.sql.*;
+import com.gustavo.contatos.database.ConnectDatabase;
 
-public class ContatoDAO    {
+
+public class ContatoDAO{
+
+    public void adicionar(Contato contato) throws SQLException{
+        Connection conect = ConnectDatabase.conectar();
+        PreparedStatement pstmt = null;
+
+        try{
+            String sql = "INSERT INTO Contato(nome, telefone, email) VALUES (?,?,?)";
+            pstmt = conect.prepareStatement(sql);
+            pstmt.setString(1, contato.getNome());
+            pstmt.setString(2, contato.getEmail());
+            pstmt.setString(3, contato.getTelefone());
+            pstmt.executeUpdate();
+            System.out.println("Contato adicionado com sucesso!");
+        } finally {
+            if(pstmt != null){
+                pstmt.close();
+            }
+            if (conect != null){
+                conect.close();
+            }
+        }
+    }
+
 }
