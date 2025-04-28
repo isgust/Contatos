2adcfe24fa3c14b3b2604bede8acc23daecbc964
isgust <rocha.gustavops@gmail.com>
2025-04-28 04:32:37 -0300

    Metodo de Delete e correção no metodo de pesquisa(add na lista no final do while)

--- src/main/java/com/gustavo/contatos/model/Contato.java
+++ src/main/java/com/gustavo/contatos/model/Contato.java
@@ -18,6 +18,10 @@ public class Contato {
     }
 
     //Getters
+    public int getId(){
+        return id;
+    }
+
     public String getNome(){
         return nome;
     }
@@ -31,6 +35,10 @@ public class Contato {
     }
 
     //Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public void setNome(String nome){
         this.nome = nome;
     }
--- src/main/java/com/gustavo/contatos/model/ContatoDAO.java
+++ src/main/java/com/gustavo/contatos/model/ContatoDAO.java
@@ -7,18 +7,19 @@ import com.gustavo.contatos.database.ConnectDatabase;
 
 
 public class ContatoDAO{
-
+    //Metodo para adicionar Contatos
     public void adicionar(Contato contato) throws SQLException{
         Connection conect = null;
         PreparedStatement pstmt = null;
 
         try{
             conect = ConnectDatabase.conectar();
-            String sql = "INSERT INTO Contato(nome, telefone, email) VALUES (?,?,?)";
+            String sql = "INSERT INTO Contato(id, nome, telefone, email) VALUES (?,?,?,?)";
             pstmt = conect.prepareStatement(sql);
-            pstmt.setString(1, contato.getNome());
-            pstmt.setString(2, contato.getEmail());
-            pstmt.setString(3, contato.getTelefone());
+            pstmt.setInt(1, contato.getId());
+            pstmt.setString(2, contato.getNome());
+            pstmt.setString(3, contato.getEmail());
+            pstmt.setString(4, contato.getTelefone());
             pstmt.executeUpdate();
             System.out.println("Contato adicionado com sucesso!");
         } finally {
@@ -31,6 +32,7 @@ public class ContatoDAO{
         }
     }
 
+    //Metodo para Listar contatos
     public List<Contato> listarContatos() throws SQLException{
         Connection conect = null;
         PreparedStatement pstmt = null;
@@ -39,15 +41,17 @@ public class ContatoDAO{
 
         try {
         conect = ConnectDatabase.conectar();
-        String sql = "SELECT nome, telefone, email FROM Contato";
+        String sql = "SELECT id, nome, telefone, email FROM Contato";
         pstmt = conect.prepareStatement(sql);
         rs = pstmt.executeQuery();
 
         while (rs.next()){
             Contato contato = new Contato();
+            contato.setId(rs.getInt("id"));
             contato.setNome(rs.getString("nome"));
             contato.setTelefone(rs.getString("telefone"));
             contato.setEmail(rs.getString("email"));
+            contatos.add(contato); // Adiciona os contatos a Lista
         }
 
         } finally {
@@ -62,9 +66,45 @@ public class ContatoDAO{
             }
 
         }
-        return contatos;
+        return contatos; //Retorna a Lista
     }
 
+    //Metodo para Pesquisar Contatos(Usando o metodo Listar contatos como base)
+    public List<Contato> pesquisarContatos(String criterio) throws SQLException{
+        List<Contato> todosContatos = listarContatos(); //Chama o metodo com a lista de contatos
+        List<Contato> filtrados = new ArrayList<>();
 
+        for (Contato contato: todosContatos){
+            if (contato.getNome().toLowerCase().contains(criterio.toLowerCase()) || contato.getTelefone().toLowerCase().contains(criterio.toLowerCase())){
+                filtrados.add(contato);
+            }
+        }
+        return filtrados;
+    }
+
+    //Metodo de Deletar contatos
+    public void deletar(int enterId) throws SQLException{
+        Connection conect = null;
+        PreparedStatement pstmt = null;
+        try{
+            conect = ConnectDatabase.conectar();
+            String sql = "DELETE FROM Contato WHERE id = ?";
+            pstmt = conect.prepareStatement(sql);
+            pstmt.setInt(1, enterId);
+            pstmt.executeUpdate();
+            System.out.println("Contato Excluido com sucesso!");
+
+
+        }finally {
+            if (conect != null){
+                conect.close();
+            }
+            if (pstmt != null){
+                pstmt.close();
+            }
+
+        }
+
+    }
 
 }
